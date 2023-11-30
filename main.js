8eaa255ded305a0e542b6b3827b2abe2e4e91a16
Leticia1508 <leticiademoraes0@gmail.com>
2023-11-30 00:53:39 -0300

    Teclado numerico e som

--- main.js
+++ main.js
@@ -25,7 +25,7 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
     }
 
     tecla.onkeydown = function (evento) {
-
+        console.log(evento)
         if (evento.code === 'Space' || evento.code === 'Enter') {
             tecla.classList.add('ativa');
         }
@@ -37,3 +37,23 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
     }
 
 }
+
+
+const TECLADO_E_SOM = {
+    Numpad7: '#som_tecla_pom',
+    Numpad8: '#som_tecla_clap',
+    Numpad9: '#som_tecla_tim',
+
+    Numpad4: '#som_tecla_puff',
+    Numpad5: '#som_tecla_splash',
+    Numpad6: '#som_tecla_toim',
+    
+    Numpad1: '#som_tecla_psh',
+    Numpad2: '#som_tecla_tic',
+    Numpad3: '#som_tecla_tom',
+}
+
+document.addEventListener('keydown', function(evento) {
+    console.log(evento.code);
+    tocaSom(TECLADO_E_SOM[evento.code]);
+});
